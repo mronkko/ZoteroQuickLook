747bcf143929d3f0a9a3984fb6e5cf81be40acf6
bwiernik <github@wiernik.org>
2017-11-29 12:56:43 +0100

    Fix bug from last commit

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ mv install.rdf install-base.rdf
 mv install-altwindows.rdf install.rdf
 zip -r builds/zoteroquicklook-altwindows.xpi chrome defaults install.rdf chrome.manifest -x .*
 mv install.rdf install-altwindows.rdf
-mv install-base.rdf install-altwindows.rdf
+mv install-base.rdf install.rdf
