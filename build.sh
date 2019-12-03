d776a2ef87fb9ed6a87589c3b10993af343e4585
Brenton M. Wiernik <bwiernik@users.noreply.github.com>
2019-12-03 13:28:51 -0500

    Prep for 1.4.0 release

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-zip -r builds/zoteroquicklook.zotero.plugin chrome defaults install.rdf chrome.manifest -x .*
+zip -r builds/zoteroquicklook.zoteroplugin chrome defaults install.rdf chrome.manifest -x .*
 mv install.rdf install-base.rdf
 mv install-altwindows.rdf install.rdf
-zip -r builds/zoteroquicklook-altwindows.zotero.plugin chrome defaults install.rdf chrome.manifest -x .*
+zip -r builds/zoteroquicklook-altwindows.zoteroplugin chrome defaults install.rdf chrome.manifest -x .*
 mv install.rdf install-altwindows.rdf
 mv install-base.rdf install.rdf
