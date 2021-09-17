b948844eee6a3bd29c3b1b817927a124a201b5b7
Tin Lai <oscar@tinyiu.com>
2021-09-17 23:29:03 +1000

    retab to be consistent
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

--- chrome/content/zoteroquicklook.pl
+++ chrome/content/zoteroquicklook.pl
@@ -30,13 +30,13 @@ foreach $argnum (0 .. $#ARGV) {
 
 
 if (scalar @files > 0) {
-  if (-e '/usr/bin/qlmanage') { exec('/usr/bin/qlmanage','-p',@files); }
-  elsif (-e '/usr/bin/sushi') {
-	# we pass an entire string, instead of a list of args, to `system` because dbus-send needs to run in an actual shell
-    $cmd = 'dbus-send --print-reply --dest=org.gnome.NautilusPreviewer /org/gnome/NautilusPreviewer org.gnome.NautilusPreviewer.ShowFile string:"file://%s" int32:0 boolean:false';
-  	# sushi only support viewing single file
-    system sprintf($cmd,File::Spec->rel2abs($files[0]));
-  }
-  elsif (-e '/usr/bin/gloobus-preview') { system('/usr/bin/gloobus-preview',@files); }
-  else { print "No QuickLook application found"; }
+	if (-e '/usr/bin/qlmanage') { exec('/usr/bin/qlmanage','-p',@files); }
+	elsif (-e '/usr/bin/sushi') {
+		# we pass an entire string, instead of a list of args, to `system` because dbus-send needs to run in an actual shell
+		$cmd = 'dbus-send --print-reply --dest=org.gnome.NautilusPreviewer /org/gnome/NautilusPreviewer org.gnome.NautilusPreviewer.ShowFile string:"file://%s" int32:0 boolean:false';
+		# sushi only support viewing single file
+		system sprintf($cmd,File::Spec->rel2abs($files[0]));
+	}
+	elsif (-e '/usr/bin/gloobus-preview') { system('/usr/bin/gloobus-preview',@files); }
+	else { print "No QuickLook application found"; }
 }
