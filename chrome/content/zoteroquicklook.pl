d69e5edd264999c2cda0fabaae632a5584aaebcb
Tin Lai <oscar@tinyiu.com>
2021-09-17 23:12:57 +1000

    preliminary support for sushi
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

--- chrome/content/zoteroquicklook.pl
+++ chrome/content/zoteroquicklook.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl
 
+use File::Spec;
+use IPC::Open2;
+
 print "\n\nThis file contains debug information\n\n";
 
 my @files=();
@@ -26,6 +29,14 @@ foreach $argnum (0 .. $#ARGV) {
 #
 
 
-if (-e '/usr/bin/qlmanage') { exec('/usr/bin/qlmanage','-p',@files); }
-elsif (-e '/usr/bin/gloobus-preview') { system('/usr/bin/gloobus-preview',@files); }
-else { print "No QuickLook application found"; }
+if (scalar @files > 0) {
+  if (-e '/usr/bin/qlmanage') { exec('/usr/bin/qlmanage','-p',@files); }
+  elsif (-e '/usr/bin/sushi') {
+	# we pass an entire string, instead of a list of args, to `system` because dbus-send needs to run in an actual shell
+    $cmd = 'dbus-send --print-reply --dest=org.gnome.NautilusPreviewer /org/gnome/NautilusPreviewer org.gnome.NautilusPreviewer.ShowFile string:"file://%s" int32:0 boolean:false';
+  	# sushi only support viewing single file
+    system sprintf($cmd,File::Spec->rel2abs($files[0]));
+  }
+  elsif (-e '/usr/bin/gloobus-preview') { system('/usr/bin/gloobus-preview',@files); }
+  else { print "No QuickLook application found"; }
+}
